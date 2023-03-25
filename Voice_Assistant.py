f1c76bfcb0ef02351c4416b69b8c7ee412e06cd7
Aditya Kumar <adityak14200@gmail.com>
2023-03-25 15:27:04 -0400

    Final Working Model

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -1,27 +1,24 @@
 import openai
-
 import speech_recognition as sr
 import pyttsx3
-import time 
-
 
 # Initialize OpenAI API
 openai.api_key = "my api key"
-# Initialize the text to speech engine 
-engine=pyttsx3.init()
 
+# Initialize the text to speech engine 
+engine = pyttsx3.init()
 
 def transcribe_audio_to_test(filename):
-    recogizer=sr.Recognizer()
-    with sr.AudioFile(filename)as source:
-        audio=recogizer.record(source) 
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(filename) as source:
+        audio = recognizer.record(source) 
     try:
-        return recogizer.recognize_google(audio)
+        return recognizer.recognize_google(audio)
     except:
-        print("skipping unkown error")
+        print("Skipping unknown error")
 
 def generate_response(prompt):
-    response= openai.completion.create(
+    response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         max_tokens=4000,
@@ -29,49 +26,45 @@ def generate_response(prompt):
         stop=None,
         temperature=0.5,
     )
-    return response ["Choices"][0]["text"]
+    return response["choices"][0]["text"]
+
 def speak_text(text):
     engine.say(text)
     engine.runAndWait()
 
 def main():
     while True:
-        #Waith for user say "genius"
+        # Wait for user to say "genius"
         print("Say 'Genius' to start recording your question")
         with sr.Microphone() as source:
-            recognizer=sr.Recognizer()
-            audio=recognizer.listen(source)
+            recognizer = sr.Recognizer()
+            audio = recognizer.listen(source)
             try:
                 transcription = recognizer.recognize_google(audio)
-                if transcription.lower()=="genius":
-                    #record audio
-                    filename ="input.wav"
+                if transcription.lower() == "genius":
+                    # Record audio
+                    filename = "input.wav"
                     print("Say your question")
                     with sr.Microphone() as source:
-                        recognizer=sr.recognize()
-                        source.pause_threshold=1
-                        audio=recognizer.listen(source,phrase_time_limit=None,timeout=None)
-                        with open(filename,"wb")as f:
+                        recognizer = sr.Recognizer()
+                        source.pause_threshold = 1
+                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
+                        with open(filename, "wb") as f:
                             f.write(audio.get_wav_data())
-                            
-                            
-                        
-                        
-                    #transcript audio to test 
-                    text=transcribe_audio_to_test(filename)
+
+                    # Transcript audio to text 
+                    text = transcribe_audio_to_test(filename)
                     if text:
-                        print(f"yuo said {text}")
-                        
-                        #Generate the response
+                        print(f"You said {text}")
+
+                        # Generate the response
                         response = generate_response(text)
-                        print(f"chat gpt 3 say {response}")
-                            
-                        #read resopnse using GPT3
+                        print(f"Chat GPT-3 says: {response}")
+
+                        # Read response using text-to-speech
                         speak_text(response)
             except Exception as e:
-                
-                print("An error ocurred : {}".format(e))
-if __name__=="__main__":
+                print(f"An error occurred: {e}")
+
+if __name__ == "__main__":
     main()
-
-
