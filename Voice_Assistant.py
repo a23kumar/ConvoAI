d07648f4fd994b3f60fbe935bb5427656bb6aaea
Aditya Kumar <adityak14200@gmail.com>
2023-03-25 15:41:50 -0400

    added functionality to close if user prompts

--- Voice_Assistant.py
+++ Voice_Assistant.py
@@ -1,24 +1,27 @@
 import openai
+
 import speech_recognition as sr
 import pyttsx3
+import time 
+
 
 # Initialize OpenAI API
-openai.api_key = "my api key"
-
+openai.api_key = "api key"
 # Initialize the text to speech engine 
-engine = pyttsx3.init()
+engine=pyttsx3.init()
+
 
 def transcribe_audio_to_test(filename):
-    recognizer = sr.Recognizer()
-    with sr.AudioFile(filename) as source:
-        audio = recognizer.record(source) 
+    recogizer=sr.Recognizer()
+    with sr.AudioFile(filename)as source:
+        audio=recogizer.record(source) 
     try:
-        return recognizer.recognize_google(audio)
+        return recogizer.recognize_google(audio)
     except:
-        print("Skipping unknown error")
+        print("skipping unkown error")
 
 def generate_response(prompt):
-    response = openai.Completion.create(
+    response= openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         max_tokens=4000,
@@ -26,45 +29,50 @@ def generate_response(prompt):
         stop=None,
         temperature=0.5,
     )
-    return response["choices"][0]["text"]
-
+    return response ["choices"][0]["text"]
 def speak_text(text):
     engine.say(text)
     engine.runAndWait()
 
 def main():
     while True:
-        # Wait for user to say "genius"
-        print("Say 'Genius' to start recording your question")
+        #Waith for user say "genius"
+        print("Say 'Genius' to start recording your question. If you would like to close the program, say 'end'")
         with sr.Microphone() as source:
-            recognizer = sr.Recognizer()
-            audio = recognizer.listen(source)
+            recognizer=sr.Recognizer()
+            audio=recognizer.listen(source)
             try:
                 transcription = recognizer.recognize_google(audio)
-                if transcription.lower() == "genius":
-                    # Record audio
-                    filename = "input.wav"
+                if transcription.lower()=="genius":
+                    #record audio
+                    filename ="input.wav"
                     print("Say your question")
                     with sr.Microphone() as source:
-                        recognizer = sr.Recognizer()
-                        source.pause_threshold = 1
-                        audio = recognizer.listen(source, phrase_time_limit=None, timeout=None)
-                        with open(filename, "wb") as f:
+                        recognizer=sr.Recognizer()
+                        source.pause_threshold=1
+                        audio=recognizer.listen(source,phrase_time_limit=None,timeout=None)
+                        with open(filename,"wb")as f:
                             f.write(audio.get_wav_data())
-
-                    # Transcript audio to text 
-                    text = transcribe_audio_to_test(filename)
+                            
+                    #transcript audio to text 
+                    text=transcribe_audio_to_test(filename)
                     if text:
-                        print(f"You said {text}")
-
-                        # Generate the response
+                        print(f"you said {text}")
+                        
+                        #Generate the response
                         response = generate_response(text)
-                        print(f"Chat GPT-3 says: {response}")
-
-                        # Read response using text-to-speech
+                        print(f"chat gpt 3 says {response}")
+                            
+                        #read response using text-to-speech
                         speak_text(response)
+                        
+                        # Check if the user wants to end the program
+                        if "close" in response.lower():
+                            print("Ending the program...")
+                            break
             except Exception as e:
-                print(f"An error occurred: {e}")
+                
+                print("An error occurred : {}".format(e))
 
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
